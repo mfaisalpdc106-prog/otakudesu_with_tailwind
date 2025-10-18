b46f9eb8cc41d93e705fcd60426f29befff38956
faisal <233119854+mfaisalpdc106-prog@users.noreply.github.com>
2025-10-18 14:18:21 +0700

    tes

--- javascript10.js
+++ javascript10.js
@@ -1,7 +1,11 @@
-function faktorial(n){
-    if(n === 0){
-        return 1;
-    }
-    return n * faktorial(n-1);
+// function faktorial(n){
+//     if(n === 0){
+//         return 1;
+//     }
+//     return n * faktorial(n-1);
+// }
+// console.log(faktorial(10));
+
+for(let i = 1; i < 10; i++){
+    console.log(i);
 }
-console.log(faktorial(10));
