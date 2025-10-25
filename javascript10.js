a44381d149530bc9f6b8d0fef0d68a1dc961e3b4
faisal <233119854+mfaisalpdc106-prog@users.noreply.github.com>
2025-10-25 17:38:21 +0700

    menambahkan file js11

--- javascript10.js
+++ javascript10.js
@@ -1,7 +1,11 @@
-function faktorial(n){
-    if(n === 0){
-        return 1;
-    }
-    return n * faktorial(n-1);
+// function faktorial(n){
+//     if(n === 0){
+//         return 1;
+//     }
+//     return n * faktorial(n-1);
+// }
+// console.log(faktorial(10));
+
+for(let i = 0; i <= 10; i = i+i){
+    console.log(i);
 }
-console.log(faktorial(10));
---
+++ javascript11.js
@@ -0,0 +1,23 @@
+// Function Declaration
+// function identifier (ParameterList opt) { FunctionBody }
+
+//Contoh :
+function tampilPesan (nama){
+    alert('halo ' + nama);
+}
+
+console.log(tampilPesan('faisal'));
+// Function Expression
+// function identifier opt (ParameterList opt) { FunctionBody }
+
+//Contoh
+// let tampilPesan = function (nama){
+//     alert('halo' + nama);
+// }
+
+//Contoh
+// let tampil = function(name){
+//     alert('halo ' + name);
+// }
+
+// tampil('faisal');
