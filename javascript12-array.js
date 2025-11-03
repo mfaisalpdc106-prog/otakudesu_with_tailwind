68e6c9e0e7118bcc08c8be011d22e3441a6e0284
faisal <233119854+mfaisalpdc106-prog@users.noreply.github.com>
2025-11-03 17:22:51 +0700

    #

---
+++ javascript12-array.js
@@ -0,0 +1,24 @@
+// let arr = ['a', 1, true];
+// console.log(arr[1]);
+
+
+//menambah isi array
+// let arr = [];
+// arr[0] = 'array1';
+// arr[1] = 'array2';
+// arr[2] = 'array3';
+// arr[4] = 'array4';
+
+// console.log(arr);
+
+//menghapus isi array (undifined)
+// let arr = ['array1', 'array2', 'array3'];
+// arr[1] = undefined;
+// console.log(arr);
+
+//menampilkan isi array dengan looping
+let arr = ['array1', 'array2', 'array3'];
+
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
