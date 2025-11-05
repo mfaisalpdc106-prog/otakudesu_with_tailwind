ad04860e8b4d2f5739990779d22c1b543f0e8282
faisal <233119854+mfaisalpdc106-prog@users.noreply.github.com>
2025-11-05 17:24:51 +0700

    menambahkan javascript13

---
+++ javascript13-latihan-array&function.js
@@ -0,0 +1,21 @@
+let penumpang = ['adit', undefined, 'ari'];
+
+let tambahPenumpang = function(namaPenumpang, penumpang){
+    if(penumpang.length == 0){
+        penumpang.push(namaPenumpang);
+        return penumpang;
+    }else{
+        for(let i = 0; i < penumpang.length; i++){
+            if(penumpang[i] == undefined){
+                penumpang[i] = namaPenumpang;
+                return penumpang;
+            }else if(penumpang[i] == namaPenumpang){
+                console.log(namaPenumpang + ' sudah ada di dalam angkot.');
+                return penumpang;
+            }else if (i == penumpang.length - 1){
+                penumpang.push(namaPenumpang);
+                return penumpang;
+            }
+        }
+    }
+}
