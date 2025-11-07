037ba0e6fdc332b0e8e15773ad3603cfd089b30c
faisal <233119854+mfaisalpdc106-prog@users.noreply.github.com>
2025-11-07 17:18:13 +0700

    #

--- javascript14-object.js
+++ javascript14-object.js
@@ -1,7 +1,7 @@
 let mhs = {
     nama : 'adit',
     umur : 30,
-    ips : [3.00, 2.50, 3.20],
+    ips : [3.00, 2.50, 3.20],              
     alamat : {
         jalan : 'jl.anwar',
         kota : 'bandung',
