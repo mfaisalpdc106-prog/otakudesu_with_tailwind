776c9e4dafadf13153c3c99207280593c3aaddd7
faisal <233119854+mfaisalpdc106-prog@users.noreply.github.com>
2025-11-18 15:56:46 +0700

    memodifikasi beberapa file latian object dan tugas sistem pakar

--- javascript15-latihan-object.js
+++ javascript15-latihan-object.js
@@ -4,6 +4,27 @@ function Angkot(sopir, trayek, penumpang, kas){
     this.trayek = trayek;
     this.penumpang = penumpang;
     this.kas = kas;
+
+
+    this.penumpangNaik = function(namaPenumpang){
+        this.penumpang.push(namaPenumpang);
+        return this.penumpang;
+    }
+
+    this.penumpangTurun = function(namaPenumpang, bayar){
+        if(this.penumpang.length === 0){
+            alert('angkot masih kosong!');
+            return false;
+        }
+
+        for(let i = 0; i < this.penumpang.length; i ++){
+            if(this.penumpang[i] == namaPenumpang){
+                this.penumpang[i] = undefined;
+                this.kas += bayar;
+                return this.penumpang;
+            }
+        }
+    }
 }
 
 let angkot1 = new Angkot('Adit', ['Blitar', 'Kediri'], [], 0);
